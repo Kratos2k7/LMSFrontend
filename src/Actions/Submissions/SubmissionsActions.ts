11dc570336a24a0123f57eacc326c5f7ac733331
Kratos2k7 <umernadeem321@gmail.com>
2024-04-10 13:43:18 +0500

    Refactor submission actions and API utils, and update token handling

--- src/Actions/Submissions/SubmissionsActions.ts
+++ src/Actions/Submissions/SubmissionsActions.ts
@@ -4,17 +4,11 @@ const getAllSubmissions = async (data: any) =>
   await http.post(
     `${
       import.meta.env.VITE_LMS_BACKEND_URL
-    }submissions/${data.courseId}/${data.assignmentId}?token=${
-      localStorage.getItem('token')
-        ? localStorage.getItem('token')
-        : import.meta.env.TOKEN
-    }`
+    }submissions/${data.courseId}/${data.assignmentId}?token=${import.meta.env.TOKEN}`
   )
 const uploadSubmissionsFile = async (data: any, payload: any) => {
   Object.assign(payload, {
-    token: localStorage.getItem('token')
-      ? localStorage.getItem('token')
-      : import.meta.env.TOKEN,
+    token: import.meta.env.TOKEN,
   })
   return await http.post(
     `${
@@ -27,41 +21,31 @@ const addSubmissionComment = async (data: any, payload: any) =>
   await http.put(
     `${
       import.meta.env.VITE_LMS_BACKEND_URL
-    }submissions/${data.courseId}/${data.assignmentId}/${data.userId}?token=${localStorage.getItem(
-      'token'
-    ) ? localStorage.getItem('token') : import.meta.env.TOKEN}&comment=${payload}`
+    }submissions/${data.courseId}/${data.assignmentId}/${data.userId}?token=${import.meta.env.TOKEN}&comment=${payload}`
   )
 const addSubmissionGrade = async (data: any, payload: any) =>
   await http.put(
     `${
       import.meta.env.VITE_LMS_BACKEND_URL
-    }submissions/${data.courseId}/${data.assignmentId}/${data.userId}/grade?token=${localStorage.getItem(
-      'token'
-    ) ? localStorage.getItem('token') : import.meta.env.TOKEN}&grade=${payload}`
+    }submissions/${data.courseId}/${data.assignmentId}/${data.userId}/grade?token=${import.meta.env.TOKEN}&grade=${payload}`
   )
 const editSubmissionComment = async (data: any, payload: any) =>
   await http.put(
     `${
       import.meta.env.VITE_LMS_BACKEND_URL
-    }submissions/${data.courseId}/${data.assignmentId}/${data.userId}/${data.commentId}?token=${localStorage.getItem(
-      'token'
-    ) ? localStorage.getItem('token') : import.meta.env.TOKEN}&comment=${payload}`
+    }submissions/${data.courseId}/${data.assignmentId}/${data.userId}/${data.commentId}?token=${import.meta.env.TOKEN}&comment=${payload}`
   )
 const uploadFileSubmissionComment = async (data: any, payload: any) =>
   await http.put(
     `${
       import.meta.env.VITE_LMS_BACKEND_URL
-    }submissions/${data.courseId}/${data.assignmentId}/${data.userId}/upload?token=${localStorage.getItem(
-      'token'
-    ) ? localStorage.getItem('token') : import.meta.env.TOKEN}&comment=${payload}`
+    }submissions/${data.courseId}/${data.assignmentId}/${data.userId}/upload?token=${import.meta.env.TOKEN}&comment=${payload}`
   )
 const deleteSubmissionComment = async (data: any) =>
   await http.delete(
     `${
       import.meta.env.VITE_LMS_BACKEND_URL
-    }submissions/${data.courseId}/${data.assignmentId}/${data.submissionId}/${data.commentId}?token=${localStorage.getItem(
-      'token'
-    ) ? localStorage.getItem('token') : import.meta.env.TOKEN}`
+    }submissions/${data.courseId}/${data.assignmentId}/${data.submissionId}/${data.commentId}?token=${import.meta.env.TOKEN}`
   )
 
 export {
